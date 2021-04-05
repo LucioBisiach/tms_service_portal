10c3d0f5ba2cc15d6e2be8b0f842b4723d547630
BisiachLucio <l.bisiach@gmail.com>
2021-04-05 19:13:04 +0000

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'TMS Services Portal',
+    'version': '1.0',
+    'category': 'Info Supplier',
+    'depends': ['portal','tms'],
+    'data': [
+        'portal_view.xml',
+        'security/ir.model.access.csv',
+
+    ],
+    'demo': [
+        ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
